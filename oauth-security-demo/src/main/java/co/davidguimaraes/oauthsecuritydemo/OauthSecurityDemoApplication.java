a2016b5968e42f6f3da2a7b19637f13b27c645a2
David Guimaraes <david.ce@live.com>
2020-04-02 19:44:39 -0400

    01. Initializing project.

---
+++ oauth-security-demo/src/main/java/co/davidguimaraes/oauthsecuritydemo/OauthSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package co.davidguimaraes.oauthsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSecurityDemoApplication.class, args);
+	}
+
+}
